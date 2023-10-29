d5cbae56d34401a8a17a53ce266d2c5460f1886f
siwinter <45730097+siwinter@users.noreply.github.com>
2023-10-29 19:24:01 +0100

    new way to use GStreamer from python environment

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@
 pkg_dependencies="acl build-essential python3-dev python3-pip python3-venv git postgresql postgresql-contrib \
 gir1.2-gst-plugins-base-1.0 gir1.2-gstreamer-1.0 gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly lsb-base \
 python3-gst-1.0 python3-tornado debconf python3-pkg-resources python3-pykka python3-requests \
-gstreamer1.0-alsa gstreamer1.0-pulseaudio gstreamer1.0-tools"
+gstreamer1.0-alsa gstreamer1.0-pulseaudio gstreamer1.0-tools libgirepository1.0-dev"
 
 #=================================================
 # PERSONAL HELPERS
@@ -28,8 +28,9 @@ myynh_install() {
 	ynh_exec_as $app $final_path/env/bin/pip install --upgrade --no-cache-dir pip
 
 	# to make Gstreamer visible in Python environment
-	$final_path/env/bin/python3 -m pip install vext
-	$final_path/env/bin/python3 -m pip install --no-binary=:all: vext.gi
+#	$final_path/env/bin/python3 -m pip install vext
+#	$final_path/env/bin/python3 -m pip install --no-binary=:all: vext.gi
+	$final_path/env/bin/python3 -m pip install PyGObject
 
 	# install essential packages
 	ynh_exec_as $app $final_path/env/bin/python3 -m pip install --no-cache-dir Mopidy==$(ynh_app_upstream_version)
